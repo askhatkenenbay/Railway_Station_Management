17eb0dd8b666c41db60a531e9c953448c1bf309d
Askhat Kenenbay <Askhat_Kenenbay@epam.com>
2019-10-17 13:57:12 +0500

    getStation added

--- src/main/java/railway_system/dao/BaseDaoImpl.java
+++ src/main/java/railway_system/dao/BaseDaoImpl.java
@@ -1,13 +1,35 @@
 package railway_system.dao;
 
+import railway_system.connection.ConnectionPool;
 import railway_system.entity.Station;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class BaseDaoImpl implements BaseDao {
+    private static final String SELECT_CITY_NAME_ID_FROM_STATION = "SELECT city,name,station_id from station";
 
     public ArrayList<Station> getAllStations(){
-
+        Connection connection = null;
+        ArrayList<Station> resultList = null;
+        try{
+            connection = ConnectionPool.INSTANCE.getConnection();
+            resultList = new ArrayList<>();
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_CITY_NAME_ID_FROM_STATION);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                resultList.add(new Station(resultSet.getString("city"),
+                        resultSet.getString("name"),resultSet.getInt("station_id")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close(connection);
+        }
+       return resultList;
     }
 
 }
--- src/main/java/railway_system/server/StationService.java
+++ src/main/java/railway_system/server/StationService.java
@@ -1,9 +1,14 @@
 package railway_system.server;
 
 
+import com.google.gson.Gson;
+import railway_system.dao.BaseDaoImpl;
+import railway_system.entity.Station;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 
 @Path("/station")
 public class StationService {
@@ -15,6 +20,13 @@ public class StationService {
     @GET
     public Response getAll(){
         //return all stations
+        Gson gson = new Gson();
+        ArrayList<Station> stations = new BaseDaoImpl().getAllStations();
+        String json = gson.toJson(stations, ArrayList<>.class);
+
+
+
+
         return Response.ok().build();
     }
 }
